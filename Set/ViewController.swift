2ce3443a1b53cb9772a6bb6585b6c27e5bb3bad9
Guga <ggong13@freeuni.edu.ge>
2019-11-24 16:00:48 +0400

    feat: basic game UI

---
+++ Set/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Guga Gongadze on 11/20/19.
+//  Copyright © 2019 Guga Gongadze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func onDealMoreCards(_ sender: UIButton) {
+        print("Deal 3 More Cards!")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
